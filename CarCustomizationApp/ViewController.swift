5240ddb0adf26c318cd5296da100b2fa1ecae06a
Kewal Patel <kaakaa135@gmail.com>
2021-11-10 13:27:44 -0500

    For Midterm

--- CarCustomizationApp/ViewController.swift
+++ CarCustomizationApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBAction func Sedan(_ sender: UIButton) {
+        
+        image.image = UIImage(named: "Sedan")
+        
     }
+    
+    @IBAction func SUV(_ sender: UIButton) {
+        
+        image.image = UIImage(named: "SUV")
 
-
+    }
+    
+    @IBAction func Truck(_ sender: UIButton) {
+        
+        image.image = UIImage(named: "Truck")
+        
+    }
+    
+    @IBAction func Hatchback(_ sender: UIButton) {
+        
+        image.image = UIImage(named: "Hatchback")
+        
+    }
+    
+    
 }
 
